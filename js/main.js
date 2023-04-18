ac913f81b60fb00b1ce50be4376c719e8dc11a24
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-18 08:49:02 +0200

    feat(aggiunta colori e modifiche di style)

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const contentitore = document.getElementById("container");
 for (let num = 0; num < 101; num++) {
     
     if (num %3==0  && num %5==0 ) {
-        contentitore.innerHTML += `<div class="box">${"FixBuzz"}</div>`;
+        contentitore.innerHTML += `<div class="box1">${"Fizzbuzz"}</div>`;
         
         console.log (num + "fixxzBBBUUUZ");
     }else if (num %3==0){
-        contentitore.innerHTML += `<div class="box">${"Buzz"}</div>`;
+        contentitore.innerHTML += `<div class="box2">${"Buzz"}</div>`;
         console.log (num +"buz" );
     }else if ( num %5==0){
-        contentitore.innerHTML += `<div class="box">${"Fizz"}</div>`;
+        contentitore.innerHTML += `<div class="box3">${"Fizz"}</div>`;
         console.log (num + "fixxz");
     }else{
         contentitore.innerHTML += `<div class="box">${num}</div>`;
